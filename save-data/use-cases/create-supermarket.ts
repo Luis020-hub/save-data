0c1e5c26da690e0fb3163609bcff808374e5f6e3
Luís Gustavo Monteiro Santos <guhmonteiro10@hotmail.com>
2024-08-03 09:39:34 -0300

    Adiciona a geolocalizacao e  cria a entidade de supermarket.

--- save-data/use-cases/create-supermarket.ts
+++ save-data/use-cases/create-supermarket.ts
@@ -1,7 +1,19 @@
+type Input = {
+    cnpj: string;
+    name: string;
+    address: string;
+}
+
 export class CreateSupermarket {
-    public constructor() { }
+    public constructor(readonly supermarketRepository) { }
 
-    public async execute(): Promise<void> {
-
+    public async execute({ cnpj, name, address }: Input): Promise<void> {
+        const supermarketExists = this.supermarketRepository.existsByCnpnj(cnpj);
+        if (supermarketExists) {
+            return;
+        }
+        const supermarket = Supermarket.create(cnpj, name, address);
+        await this.supermarketRepository.save(supermarket);
+        return;
     }
 }
